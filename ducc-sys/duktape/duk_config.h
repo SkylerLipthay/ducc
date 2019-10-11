20752c9de8e9b738213fef2f51ed1c4dc2320a3b
Skyler Lipthay <sl@skylerlipthay.com>
2019-10-11 06:22:46 -0600

    Increase Windows compatibility for `ducc-sys`

--- ducc-sys/duktape/duk_config.h
+++ ducc-sys/duktape/duk_config.h
@@ -8,6 +8,15 @@
 
 #include "duk_config_default.h"
 
+// Setting this flag tells Duktape to always compile using Win32's
+// `GetSystemTime` instead of `GetSystemTimePreciseAsFileTime`. The latter API
+// is only available after Windows 7. Unfortunately we have to sacrifice
+// sub-millisecond timer resolution, but it's worth it for the sake of a simple
+// interface and increased compatibility.
+#if defined(DUK_F_WINDOWS)
+#define DUK_USE_DATE_NOW_WINDOWS
+#endif
+
 #ifdef RUST_DUK_USE_EXEC_TIMEOUT_CHECK
 #define DUK_USE_INTERRUPT_COUNTER
 #define DUK_USE_EXEC_TIMEOUT_CHECK(udata) \
