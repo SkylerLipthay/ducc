c6528290d9a6d382505a4c18ba53c51ace388330
wyozi <joonas.mertanen@gmail.com>
2020-07-31 19:27:56 +0800

    Add Duktape upgrade instructions and a OS X fix

--- ducc-sys/src/ffi_gen.rs
+++ ducc-sys/src/ffi_gen.rs
@@ -1,11 +1,16 @@
 extern crate bindgen;
 
 fn main() {
-    bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
         .header("duktape/wrapper.h")
         .clang_arg("-Iduktape")
-        .clang_arg("-std=c99")
-        .trust_clang_mangling(false)
+        .clang_arg("-std=c99");
+
+    if let Ok(sdk_path) = std::env::var("DUCC_SYSTEM_SDK_PATH") {
+        builder = builder.clang_args(&["-isysroot", &sdk_path]);
+    }
+
+    builder.trust_clang_mangling(false)
         .rust_target(bindgen::RustTarget::Stable_1_25)
         .whitelist_type("^(?:rust_)?duk_.*")
         .whitelist_function("^(?:rust_)?duk_.*")
